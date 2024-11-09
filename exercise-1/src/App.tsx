b1bb3dca30834086fec60395adda321f3acc6722
black_bass <black_bass.exe@hotmail.com>
2024-11-09 22:47:12 +0900

    initial commit

---
+++ exercise-1/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />;
+    </>
+  );
+}
+
+export default App;
---
+++ exercise-1/src/components/MainContent.tsx
@@ -0,0 +1,12 @@
+const MainContent = () => {
+  return (
+    <>
+      <main>
+        <h2>MAin Content</h2>
+        <p>Text of your choice</p>
+      </main>
+    </>
+  );
+};
+
+export default MainContent;
