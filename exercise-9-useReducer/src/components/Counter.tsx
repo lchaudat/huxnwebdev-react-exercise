0641a831c4466c2644bf2ac605b9ea0dbe94a82d
black_bass <black_bass.exe@hotmail.com>
2024-11-30 23:38:58 +0900

    useReducter exercise

--- exercise-9-useReducer/src/components/Counter.tsx
+++ exercise-9-useReducer/src/components/Counter.tsx
@@ -5,12 +5,51 @@ const Counter = () => {
   const [state, dispatch] = useReducer(counterReducer, initialState);
   const [inputValue, setInputValue] = useState(0);
 
-  counterReducer(initialState,dispatch){
-
-  }
+  const handleIncrement = () => dispatch({ type: "increment", payload: 1 });
+  const handleDecrement = () => dispatch({ type: "decrement", payload: 1 });
 
   return (
     <div>
+      <h1>{state.count}</h1>
+
+      <input
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(Number(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          handleIncrement();
+        }}
+      >
+        increment
+      </button>
+
+      <button
+        onClick={() => {
+          handleDecrement();
+        }}
+      >
+        decrement
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "incrementBy", payload: inputValue });
+          setInputValue(0);
+        }}
+      >
+        incrementby {inputValue}
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "decrementBy", payload: inputValue });
+          setInputValue(0);
+        }}
+      >
+        decrementBy {inputValue}
+      </button>
     </div>
   );
 };
--- exercise-9-useReducer/src/counterReducer.tsx
+++ exercise-9-useReducer/src/counterReducer.tsx
@@ -1,13 +1,35 @@
-const initialState = 0;
+// enum CountActionKind {
+//   INCREASE = "INCREASE",
+//   DECREASE = "DECREASE",
+// }
 
-const counterReducer = (state, action) => {
-  switch (action) {
-    case action.type == "increment":
-      return { ...state, count: state.count + 1 };
-      break;
+interface State {
+  count: number;
+}
+
+interface Action {
+  type: string;
+  payload: number;
+}
+
+const initialState = { count: 0 };
+
+const counterReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+
+    case "decrement":
+      return { count: state.count - 1 };
+
+    case "incrementBy":
+      return { count: state.count + action.payload };
+
+    case "decrementBy":
+      return { count: state.count - action.payload };
 
     default:
-      break;
+      return state;
   }
 };
 
